ad4cc230647e69820d6bfa04a6a4ba13be6f35f5
Benjamin <benj.bach@googlemail.com>
2016-12-13 18:56:01 -0500

    add cubix

--- Assets/Source/d3holo/DataObject.cs
+++ Assets/Source/d3holo/DataObject.cs
@@ -1,11 +1,49 @@
 ﻿using System;
-
+using System.Collections;
+using System.Collections.Generic;
 
 public class DataObject{
 	
 	public string name;
+	public Dictionary<string, object> attributes = new Dictionary<string, object>();
 
 	public DataObject(){}
 
+
+	public int Int (string attributeName)
+	{
+		int val;
+		try{
+			val = (int) attributes[attributeName];
+		}
+		catch(System.Exception ex) {
+			val = 0;
+		}  		
+		return val;
+	}
+
+	public float Float (string attributeName)
+	{
+		float val;
+		try{
+			val = (float) attributes[attributeName];
+		}
+		catch(System.Exception ex) {
+			val = 0;
+		}  		
+		return val;
+	}
+
+	public string String (string attributeName)
+	{
+		string val;
+		try{
+			val = (string) attributes[attributeName];
+		}
+		catch(System.Exception ex) {
+			val = "";
+		}  		
+		return val;
+	}
 
 }

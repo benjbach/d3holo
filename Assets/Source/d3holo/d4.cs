c551277873ac5757d010adbf6fd9006920fd2958
Benjamin <benj.bach@googlemail.com>
2016-12-19 17:59:04 -0500

    add new files

--- Assets/Source/d3holo/d4.cs
+++ Assets/Source/d3holo/d4.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.ComponentModel;
 using UnityEngine;
-using DataLoader;
 
 using System;
 using System.Collections.Generic;
@@ -14,46 +13,8 @@ public class d4: MonoBehaviour {
 
     public void Start()
     {
-        // DataLoader.DataObject do_ = new DataLoader.DataObject();
-        // Debug.Log(do_.loadCSV(myFile.text));
-   
+
     }
-
-    //void loadCSV(string data)
-    //{
-    //    string[] lines = data.Split('\n');
-
-    //    //1: read types
-    //    string[] identifiers = lines[0].Split(',');
-    //    string[] typesToRead = lines[1].Split(',');
-    //    string[] theTypes = new string[typesToRead.Length];
-    //    for(int i=0; i<typesToRead.Length;i++)
-    //    {
-    //        if (isBool(typesToRead[i]))
-    //            theTypes[i] = "bool";
-    //        else if (isFloat(typesToRead[i]))
-    //            theTypes[i] = "float";
-    //        else theTypes[i] = "string";
-    //    }
-
-    //    string DataObject = createClass(identifiers, theTypes);
-    //    //compile the class
-    //    CodeDomProvider oCodeDomProvider = CodeDomProvider.CreateProvider("CSharp");
-    //    // Add what referenced assemblies
-    //    CompilerParameters oCompilerParameters = new CompilerParameters();
-    //    oCompilerParameters.ReferencedAssemblies.Add("system.dll");
-    //    // set the compiler to create a DLL
-    //    oCompilerParameters.GenerateExecutable = false;
-    //    // set the dll to be created in memory and not on the hard drive
-    //    oCompilerParameters.GenerateInMemory = true;
-    //    var oCompilerResults =
-    //      oCodeDomProvider.CompileAssemblyFromSource(oCompilerParameters, DataObject);
-    //    if (oCompilerResults.Errors.Count != 0)
-    //        Debug.Log("There were errors while compiling the Data ");
-
-    //    Debug.Log(DataObject);
-
-    //}
 
     internal static Selection selectAll()
     {
